2f7988574ac7738325062ccc30041c3efcb9d762
zelvet-velvet <zelvet0522@gmail.com>
2022-05-12 13:56:34 +0800

    update

--- Tello/test.py
+++ Tello/test.py
@@ -5,38 +5,38 @@ tello = Tello()
 tello.connect(False)
 
 print("Press enter to continue")
-a=input( end = '')
+a=input()
 
 print("Press enter to takeoff")
 a=input()
-tello.takeoff(end = '')
+tello.takeoff()
 
 
 print("Press enter to flip forward")
-a=input(end = '')
-tello.flip(self,f)			# direction: l (left), r (right), f (forward) or b (back)
+a=input()
+tello.flip(f)			# direction: l (left), r (right), f (forward) or b (back)
 
 
 print("Press enter to move forward")
-a=input(end = '')
+a=input()
 tello.move_forward(180) 	# (cm)
 
 print("Press enter to move up")
-a=input(end = '')
-tello.move(self,up, 50)
+a=input()
+tello.move(up, 50)
 """direction: up, down, left, right, forward or back
    x: 20-500				"""
 
 print("Press enter to rotate counter clockwise")
-a=input(end = '')
+a=input()
 tello.rotate_counter_clockwise(360)	#degree
 
 print("Press enter to flip backward")
-a=input(end = '')
-tello.flip(self,b)
+a=input()
+tello.flip(b)
 
 print("Press enter to land")
-a=input(end = '')
+a=input()
 tello.land()
 
 
